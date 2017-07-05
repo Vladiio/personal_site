7e14eda4185bc62f2752a48533ad1a4a2759d57a
Vlad <unix.vlad@yandex.ru>
2017-07-05 12:55:19 +0300

    template view for index page

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(
+        template_name="my_site/index.html"), name='index'),
 ]
