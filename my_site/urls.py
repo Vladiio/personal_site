a85374d75532e4ce0f51ed43f263301c244a1e5b
Vlad M <vladpochta5@gmail.com>
2017-07-22 16:34:28 +0300

    remove python/django backend

--- my_site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', TemplateView.as_view(
-        template_name="my_site/index.html"), name='index'),
-]
